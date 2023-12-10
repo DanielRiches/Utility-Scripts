c72d22ebdc02f6f1f3bda12bf601c2b397ccb35b
DoomPriestK <danielriches1@outlook.com>
2023-12-10 19:51:27 +0000

    Added Utils.cs
    contains different types of Raycasts and other helpful code.

---
+++ Utils.cs
@@ -0,0 +1,151 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Utils
+{
+    #if ENABLE_LEGACY_INPUT_MANAGER
+    static bool legacyInputSystem = true;
+    #else
+    static bool legacyInputSystem = false;
+    #endif
+
+    #if ENABLE_INPUT_SYSTEM
+    static bool newInputSystem = true;
+    #else
+    static bool newInputSystem = false;
+    #endif
+
+    public static Vector3 GetMouseWorldPosition3D()
+    {
+        Ray ray = MouseCursorRay3D();
+
+        if (Physics.Raycast(ray, out RaycastHit cursorWorldPosition, Mathf.Infinity))
+        {
+            return cursorWorldPosition.point;
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+
+    public static Vector3 RayCastIn3DLayerSpecific(LayerMask desiredLayer)
+    {
+        Ray ray = MouseCursorRay3D();
+
+        if (Physics.Raycast(ray, out RaycastHit cursorRayCastHit, Mathf.Infinity, desiredLayer))
+        {
+            return cursorRayCastHit.point;
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+
+    public static Vector3 RayCastIn3DTagSpecific(string tag)
+    {
+        Ray ray = MouseCursorRay3D();
+
+        if (Physics.Raycast(ray, out RaycastHit cursorRayCastHit, Mathf.Infinity) && cursorRayCastHit.transform.CompareTag(tag))
+        {
+            return cursorRayCastHit.point;
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+
+    public static Vector3 RayCastIn3DLayerAndTagSpecific(LayerMask desiredLayer, string tag)
+    {
+        Ray ray = MouseCursorRay3D();
+
+        if (Physics.Raycast(ray, out RaycastHit cursorRayCastHit, Mathf.Infinity, desiredLayer) && cursorRayCastHit.transform.CompareTag(tag))
+        {
+            return cursorRayCastHit.point;
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+
+    public static Transform GetMouseCursorObjectTransform()
+    {
+        Ray ray = MouseCursorRay3D();
+
+        if (Physics.Raycast(ray, out RaycastHit cursorObjectTransform, Mathf.Infinity))
+        {
+            return cursorObjectTransform.transform;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public static GameObject GetMouseCursorGameObject()
+    {
+        Ray ray = MouseCursorRay3D();
+
+        if (Physics.Raycast(ray, out RaycastHit cursorObject, Mathf.Infinity))
+        {
+            return cursorObject.transform.gameObject;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public static GameObject RayCastFirstObjectHitNotDefaultLayer()
+    {
+        var layerMask = Physics.DefaultRaycastLayers & ~LayerMask.GetMask(Strings.defaultl);
+
+        Ray ray = MouseCursorRay3D();
+
+        if (Physics.Raycast(ray, out RaycastHit cursorObject, Mathf.Infinity, layerMask))
+        {
+            return cursorObject.transform.gameObject;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public static GameObject RayCastFirstObjectHit()
+    {        
+        var layerMask = Physics.DefaultRaycastLayers;
+
+        Ray ray = MouseCursorRay3D();
+
+        if (Physics.Raycast(ray, out RaycastHit cursorObject, Mathf.Infinity, layerMask))
+        {
+            return cursorObject.transform.gameObject;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    // -----------------------------------------------------
+    public static Ray MouseCursorRay3D(Camera camera = null)
+    {
+        if (!camera)
+        {
+            camera = Camera.main;
+        }
+
+        if (newInputSystem && legacyInputSystem || newInputSystem && !legacyInputSystem)
+        {
+            return camera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        }
+        else
+        {
+            return camera.ScreenPointToRay(Input.mousePosition);
+        }        
+    }
+}
