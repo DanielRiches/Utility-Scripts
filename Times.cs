015b15a77b79bdc0c43b2cbda9eb63297de7cf59
DoomPriestK <danielriches1@outlook.com>
2023-12-12 16:05:52 +0000

    Create Times.cs
    For caching of times in Unity.

---
+++ Times.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Times
+{
+    public static WaitForSeconds zeroOne = new WaitForSeconds(0.1f);
+    public static WaitForSeconds zeroTwo = new WaitForSeconds(0.2f);
+    public static WaitForSeconds zeroThree = new WaitForSeconds(0.3f);
+    public static WaitForSeconds zeroFour = new WaitForSeconds(0.4f);
+    public static WaitForSeconds zeroFive = new WaitForSeconds(0.5f);
+    public static WaitForSeconds zeroSix = new WaitForSeconds(0.6f);
+    public static WaitForSeconds zeroSeven = new WaitForSeconds(0.7f);
+    public static WaitForSeconds zeroEight = new WaitForSeconds(0.8f);
+    public static WaitForSeconds zeroNine = new WaitForSeconds(0.9f);
+    public static WaitForSeconds one = new WaitForSeconds(1f);
+    public static WaitForSeconds five = new WaitForSeconds(5f);
+    public static WaitForSeconds ten = new WaitForSeconds(10f);
+}
