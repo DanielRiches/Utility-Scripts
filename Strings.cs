fe58382a3f08568633e774b5c7a1ddfa3ad9c606
DoomPriestK <danielriches1@outlook.com>
2024-09-29 12:55:40 +0100

    Added MeshCombiner

---
+++ Strings.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Strings
+{
+    // SCENES
+    public const string gameSceneName = "Infectorium";
+    public const string menuSceneName = "Menus";
+    public const string splashSceneName = "SplashScreen";
+
+    // CORE VFX
+    public const string saveOrLoad = "SaveOrLoad";
+
+    // TAGS
+    public const string gameManager = "GameManager";
+    
+
+    // Layers
+    public const string environmentLayer = "Environment";
+    public const string defLayer = "Default";
+    public const string uiLayerName = "UI";
+
+    public static string loadingProgressFormat = "\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n{0}%";
+    public const string danielRichesPresents = "\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\nDaniel Riches Presents";
+    public const string saveIconWarning = "THIS GAME USES AN AUTOSAVE FEATURE.\r\n\r\n\r\n\r\n\r\n\r\n\r\nTHE SAVE ICON WILL APPEAR\r\n AT THE BOTTOM OF THE SCREEN WHILE AUTOSAVING.\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n<color=\"Red\">DO NOT EXIT THE GAME OR SHUT DOWN YOUR SYSTEM WHILE THE SAVE ICON IS DISPLAYED</color>, AS DATA MAY BE PERMANENTLY LOST.";
+    public const string photosensitiveSeizureWarning = "<color=\"Red\">PHOTOSENSETIVE SEIZURE WARNING</color>\r\n\r\n\r\n\r\nA very small percentage of people may experience a seizure when exposed to certain visual images, including flashing lights or patterns. Even people who have no history of seizures.\r\n\r\n\r\n\r\n<color=\"Red\">Immediately stop playing and consult a doctor if you experience any symptoms.</color>\r\n\r\n\r\n\r\nParents should observe their children as children and teenagers are more likely than adults to experience these seizures.\r\n\r\n\r\n\r\nIf you or any of your relatives have a history of seizures or epilepsy, consult a doctor before playing video games.";
+}
